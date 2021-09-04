0b28b71b9ef78962e83783ed040e3834532ebb78
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2021-09-04 16:43:45 -0300

    esquematizada a função do roteiro 7 e atualizada a biblioteca

--- meu_grafo_matriz_adjacencia_dir.py
+++ meu_grafo_matriz_adjacencia_dir.py
@@ -1,3 +1,5 @@
+import math
+
 from bibgrafo.grafo_matriz_adj_dir import GrafoMatrizAdjacenciaDirecionado
 from bibgrafo.grafo_exceptions import *
 
@@ -5,6 +7,14 @@ from bibgrafo.grafo_exceptions import *
 class MeuGrafo(GrafoMatrizAdjacenciaDirecionado):
 
     def existe_caminho(self, raiz, destino):
+        """
+        Utiliza o algoritmo de Warshall para verificar se há uma caminho
+        entre os vértices passados nos parâmetros.
+        :param raiz: Vértice de onde parte a busca.
+        :param destino: Vértice onde se deseja chegar.
+        :return: Valor booleano que representa a existência desse
+        caminho.
+        """
         if raiz not in self.N:
             raise VerticeInvalidoException(f'O vértice {raiz} não é válido.')
         elif destino not in self.N:
@@ -30,3 +40,20 @@ class MeuGrafo(GrafoMatrizAdjacenciaDirecionado):
                             return True
 
         return False
+
+    def caminho_drone(self, raiz, destino, carga_inicial,
+                      carga_maxima, pontos_de_recarga):
+        """
+        Utiliza de uma adaptação do algoritmo de Dijkstra para localizar
+        o menor caminho possível de um drone percorrer levando em
+        consideração o limite de sua bateria.
+        :param raiz: Vértice de onde o drone sai.
+        :param destino: Vértice onde o drone deseja chegar.
+        :param carga_inicial: Carga inicial da bateria do drone.
+        :param carga_maxima: Carga máxima da bateria do drone.
+        :param pontos_de_recarga: Lista com os vértices de ponto de
+        recarga para reabastecimento da bateria do drone.
+        :return: False se não há caminho possível ou uma lista indicando
+        o caminho no formato [v1, a1, v2, a2,..., an, vn].
+        """
+        pass
